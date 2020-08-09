60abce45c55041c36342d9b391fed9a0fe43110e
Vlad Fofanov <xdv43@yandex.ru>
2020-08-09 17:59:46 +0300

    Task 3.10 <template v-for "product in products"></template>

---
+++ 3.10.js
@@ -0,0 +1,11 @@
+/* отличие от 3.9 только в переменной и id */
+let app3_10 = new Vue({
+    el: "#s3t10",
+    data: {
+        products: [
+            {name: 'product1', price: 100, quantity: 5},
+            {name: 'product2', price: 200, quantity: 4},
+            {name: 'product3', price: 300, quantity: 3},
+        ]
+    }
+});
