eb6da270fc2cfe13224261292fabec4556ae6d88
Vlad Fofanov <xdv43@yandex.ru>
2020-08-09 17:00:42 +0300

    Task 3.4 "v-for salary in workers"

---
+++ 3.4.js
@@ -0,0 +1,10 @@
+let app3_4 = new Vue({
+    el: "#s3t4",
+    data: {
+        workers: {
+            employee1: '100$',
+            employee2: '200$',
+            employee3: '300$',
+        }
+    }
+});
