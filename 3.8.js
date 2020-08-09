b65399c719814aafcecd721241572600d52e29f2
Vlad Fofanov <xdv43@yandex.ru>
2020-08-09 17:38:57 +0300

    Task 3.8 "v-for link in hrefs"

---
+++ 3.8.js
@@ -0,0 +1,10 @@
+let app3_8 = new Vue({
+    el: "#s3t8",
+    data: {
+        hrefs: [
+            {href: '1.html', text: 'ссылка 1'},
+            {href: '2.html', text: 'ссылка 2'},
+            {href: '3.html', text: 'ссылка 3'},
+        ]
+    }
+});
