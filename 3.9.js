9b7705176ea3a7da36c57aebe2208ed9e4173701
Vlad Fofanov <xdv43@yandex.ru>
2020-08-09 17:47:15 +0300

    Task 3.9 "v-for product in products"

---
+++ 3.9.js
@@ -0,0 +1,10 @@
+let app3_9 = new Vue({
+    el: "#s3t9",
+    data: {
+        products: [
+            {name: 'product1', price: 100, quantity: 5},
+            {name: 'product2', price: 200, quantity: 4},
+            {name: 'product3', price: 300, quantity: 3},
+        ]
+    }
+});
