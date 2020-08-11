5cee8443a1e8ec4612d9e6cabca262a228b63a60
Vlad Fofanov <xdv43@yandex.ru>
2020-08-11 19:14:28 +0300

    Task 7.14 template v-if="visible"

---
+++ 7.14.js
@@ -0,0 +1,11 @@
+let app7_14 = new Vue({
+	el: '#s7t14',
+	data: {
+        visible: true,
+    },
+    methods: {
+        hide: function() {
+            this.visible = false;
+        }
+    }
+});
