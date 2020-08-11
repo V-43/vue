65ba723828011d340417bfced4caa87f0f5c129f
Vlad Fofanov <xdv43@yandex.ru>
2020-08-11 20:16:56 +0300

    Task 8.6 v-model="newItem" and v-on:click="addItem"

---
+++ 8.6.js
@@ -0,0 +1,12 @@
+let app8_6 = new Vue({
+	el: '#s8t6',
+	data: {
+        items: 'abcdef'.split(''),
+        newItem: '',
+    },
+    methods: {
+        addItem: function() {
+            this.items.push(this.newItem);
+        },
+    }
+});
