6e15802d357b21aacd5f189783e45640569e4e93
Vlad Fofanov <xdv43@yandex.ru>
2020-08-13 18:06:01 +0300

    Task 14.3 v-for="user in users" v-bind:style="{color: user.active ? 'green' : 'red'}"

---
+++ 14.3.js
@@ -0,0 +1,10 @@
+let app14_3 = new Vue({
+	el: '#s14t3',
+	data: {
+        users: [
+            {name: 'Коля', active: true},
+            {name: 'Вася', active: true},
+            {name: 'Петя', active: false},
+        ],
+    },
+});
