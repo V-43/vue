759494050b3d23eb5c056e3e9b641dc6b99dcc19
Vlad Fofanov <xdv43@yandex.ru>
2020-08-13 21:43:40 +0300

    Task 14.8 just users array in list ul

---
+++ 14.8.js
@@ -0,0 +1,10 @@
+let app14_8 = new Vue({
+	el: '#s14t8',
+	data: {
+        users: [
+            {name: 'Коля', age: 30},
+            {name: 'Вася', age: 40},
+            {name: 'Петя', age: 50},
+        ],
+    },
+});
