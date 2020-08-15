c6aaabdc7b4793c85b9b80205e6c840032462d24
Vlad Fofanov <xdv43@yandex.ru>
2020-08-15 09:10:15 +0300

    Task 17.3 directive v-once

---
+++ 17.3.js
@@ -0,0 +1,11 @@
+let app17_3 = new Vue({
+    el: '#s17t3',
+    data: {
+        text: "alukard",
+    },
+    methods: {
+        alert: function(str) {
+            alert(str);
+        },
+    }
+});
