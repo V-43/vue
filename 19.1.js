c558f69e2dfe7fff19aa30e599c6e96f3dfc3511
Vlad Fofanov <xdv43@yandex.ru>
2020-08-15 12:50:28 +0300

    Task 19.1 editing task list

---
+++ 19.1.js
@@ -0,0 +1,11 @@
+let app19_1 = new Vue({
+	el: '#s19t1',
+	data: {
+        tasks: [
+            {text: "task1", complete: false, editing: false,},
+            {text: "task2", complete: false, editing: false,},
+            {text: "task3", complete: false, editing: false,},
+        ],
+        newTask: '',
+    },
+});
